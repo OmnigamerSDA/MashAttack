ff76463ce64370f5dcf1deadf2f80055a7015421
OmnigamerSDA <omnigameromega@yahoo.com>
2016-10-17 01:40:25 -0400

    Fixed up a bunch of stuff and made player history work.

--- MashAttack/MainWindow.xaml.cs
+++ MashAttack/MainWindow.xaml.cs
@@ -51,6 +51,7 @@ namespace MashAttack
         long downval2 = 0;
         bool onebutton = true;
         SheetsAgent sheets;
+        Label[,] statLabels = new Label[4, 6];
         //string comString;
 
         delegate void SetStatusCallback(string text);
@@ -92,16 +93,58 @@ namespace MashAttack
             updatePortList();
             //myModel = new PlotModel { Title = "Mashing Rate" };
 
-            configBox.Items.Insert(0, "10s 1B");
-            configBox.Items.Insert(1, "5s 1B");
-            configBox.Items.Insert(2, "30s 1B");
+            configBox.Items.Insert(0, "10s1B");
+            configBox.Items.Insert(1, "5s1B");
+            configBox.Items.Insert(2, "30s1B");
 
-            configBox.Items.Insert(3, "10s 2B");
-            configBox.Items.Insert(4, "5s 2B");
-            configBox.Items.Insert(5, "30s 2B");
+            configBox.Items.Insert(3, "10s2B");
+            configBox.Items.Insert(4, "5s2B");
+            configBox.Items.Insert(5, "30s2B");
+
+            inputBox.Items.Insert(0, "SNES");
+            inputBox.Items.Insert(1, "NES");
+            inputBox.Items.Insert(2, "GEN");
+            inputBox.Items.Insert(3, "ARC");
 
             configBox.SelectedIndex = 0;
+            inputBox.SelectedIndex = 0;
 
+            int i = 0;
+            int j = 0;
+
+            for(i =0;i<4;i++)
+                for(j = 0;j<6;j++)
+                {
+                    statLabels[i, j] = new Label();
+                    statLabels[i, j].FontSize = 16;
+                    statLabels[i, j].HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+                    statLabels[i, j].VerticalAlignment = System.Windows.VerticalAlignment.Center;
+                    statsGrid.Children.Add(statLabels[i, j]);
+                    System.Windows.Controls.Grid.SetRow(statLabels[i, j], j);
+                    System.Windows.Controls.Grid.SetColumn(statLabels[i, j], i);
+                }
+
+            statLabels[0, 1].Content = "Rate (Hz)";
+            statLabels[0, 3].Content = "Uptime (ms)";
+            statLabels[0, 4].Content = "Downtime (ms)";
+            statLabels[0, 5].Content = "Score";
+            statLabels[0, 2].Content = "Median (Hz)";
+
+            statLabels[2, 0].Content = "Player";
+            statLabels[1, 0].Content = "Session";
+            statLabels[3, 0].Content = "Global";
+
+            statLabels[0, 1].FontWeight = System.Windows.FontWeights.Bold;
+            statLabels[0, 3].FontWeight = System.Windows.FontWeights.Bold;
+            statLabels[0, 4].FontWeight = System.Windows.FontWeights.Bold;
+            statLabels[0, 5].FontWeight = System.Windows.FontWeights.Bold;
+            statLabels[0, 2].FontWeight = System.Windows.FontWeights.Bold;
+
+            statLabels[1, 0].FontWeight = System.Windows.FontWeights.Bold;
+            statLabels[2, 0].FontWeight = System.Windows.FontWeights.Bold;
+            statLabels[3, 0].FontWeight = System.Windows.FontWeights.Bold;
+
+            PlayerUpdate();
         }
 
         void updatePortList()
@@ -344,7 +387,8 @@ namespace MashAttack
             timerLabel.Visibility = Visibility.Visible;
             mashLabel.Visibility = Visibility.Visible;
             mashLabel.Content = "Mashes: 0";
-            scoreLabel.Visibility = Visibility.Hidden;
+            //scoreLabel.Visibility = Visibility.Hidden;
+            ClearStats();
             StartComms();
 
         }
@@ -362,60 +406,83 @@ namespace MashAttack
 
             mashes = newmashes;
             mashes2 = newmashes2;
+            Stats newstats;
             long mymed = 0;
             long mymed2 = 0;
+
+            long score = (long)CalculateScore();
 
             statusBar.Fill = Brushes.Red;
             if (onebutton)
             {
-                avgTime.Content = FormatStrings((mashes.totalTime / 1000.0) / mashes.count);
-                avgHz.Content = FormatStrings(mashes.count / (mashes.totalTime / 1000.0));
-                avgDown.Content = FormatStrings(mashes.downTotal / mashes.count);
-                avgUp.Content = FormatStrings(mashes.upTotal / mashes.count);
-                avgDown2.Content = "";
-                avgUp2.Content = "";
-                maxTime.Content = mashes.fastest;
-                minTime.Content = mashes.slowest;
-                maxRate.Content = FormatStrings(1000.0 / (mashes.fastest));
-                minRate.Content = FormatStrings(1000.0 / mashes.slowest);
                 mymed = mashes.GetMedian();
 
-                medRate.Content = FormatStrings(1000.0 / mymed);
-                medTime.Content = mymed;
+                newstats = new Stats((long)mashes.totalTime, mashes.count, (long)mashes.upTotal, (long) mashes.downTotal, 1, score,mymed);
 
-                
             }
             else
             {
-                int totalmashes = mashes.count + mashes2.count;
+                double totalmashes = mashes.count + mashes2.count;
                 double totaltime = Math.Max(mashes.totalTime, mashes2.totalTime);
-                long fastest = Math.Min(mashes.fastest, mashes2.fastest);
-                long slowest = Math.Max(mashes.slowest, mashes2.slowest);
+                double totalup = mashes.upTotal + mashes2.upTotal;
+                double totaldown = mashes.upTotal + mashes2.upTotal;
 
-                avgTime.Content = FormatStrings((totaltime / 1000.0) / totalmashes);
-                avgHz.Content = FormatStrings(totalmashes / (totaltime / 1000.0));
-                avgDown.Content = FormatStrings(mashes.downTotal / mashes.count);
-                avgUp.Content = FormatStrings(mashes.upTotal / mashes.count);
-                avgDown2.Content = FormatStrings(mashes2.downTotal / mashes2.count);
-                avgUp2.Content = FormatStrings(mashes2.upTotal / mashes2.count);
-                maxTime.Content = fastest;
-                minTime.Content = slowest;
-                maxRate.Content = FormatStrings(1000.0 / fastest);
-                minRate.Content = FormatStrings(1000.0 / slowest);
                 mymed = mashes.GetMedian();
 
-                medRate.Content = FormatStrings(1000.0 / mymed);
-
                 mymed2 = mashes2.GetMedian();
-                medTime.Content = FormatStrings(1000.0 / mymed2);
+
+                newstats = new Stats((long)totaltime, (long)totalmashes, (long)totalup, (long)totaldown, 1, score,Math.Max(mymed,mymed2));
             }
 
-            CalculateScore();
+            UpdateStatLabels(newstats);
 
             PlotResults(mymed,mymed2);
         }
 
-        private void CalculateScore()
+        private void UpdateStatLabels(Stats newstats)
+        {
+            string player = usersBox.SelectedValue.ToString();
+            string mode = configBox.SelectedValue.ToString();
+            string input = inputBox.SelectedValue.ToString();
+
+            newstats.UpdateAll(onebutton, player, mode, input);
+
+            Stats playerStats = newstats.GetPlayerData(onebutton, player, mode);
+            Stats globalStats = newstats.GetGlobal(onebutton);
+
+            statLabels[2, 1].Content = FormatStrings(playerStats.totalcount/(playerStats.totaltime/1000.0));
+            statLabels[1, 1].Content = FormatStrings(newstats.totalcount / (newstats.totaltime / 1000.0));
+            statLabels[3, 1].Content = FormatStrings(globalStats.totalcount / (globalStats.totaltime / 1000.0));
+
+            statLabels[2, 3].Content =  (playerStats.totalup)/playerStats.totalcount;
+            statLabels[1, 3].Content =  (newstats.totalup) / newstats.totalcount;
+            statLabels[3, 3].Content =  (globalStats.totalup) / globalStats.totalcount;
+
+            statLabels[2, 4].Content = (playerStats.totaldown) / playerStats.totalcount;
+            statLabels[1, 4].Content =  (newstats.totaldown) / newstats.totalcount;
+            statLabels[3, 4].Content =  (globalStats.totaldown) / globalStats.totalcount;
+
+            statLabels[2, 5].Content = playerStats.totalscore / playerStats.totalsessions;
+            statLabels[1,5].Content = newstats.totalscore / newstats.totalsessions;
+            statLabels[3, 5].Content = globalStats.totalscore / globalStats.totalsessions;
+
+            statLabels[2, 2].Content = FormatStrings(1000.00/(playerStats.totalmedian / playerStats.totalsessions));
+            statLabels[1, 2].Content = FormatStrings(1000.00 / (newstats.totalmedian / newstats.totalsessions));
+            statLabels[3, 2].Content = FormatStrings(1000.00 / (globalStats.totalmedian / globalStats.totalsessions));
+        }
+
+        private void ClearStats()
+        {
+            int i, j;
+
+            for(i=1;i<4;i++)
+                for (j = 1; j < 6; j++)
+                {
+                    statLabels[i, j].Content = "";
+                }
+        }
+
+        private double CalculateScore()
         {
 
             double score = 0;
@@ -429,8 +496,13 @@ namespace MashAttack
                 temp = Math.Round(Math.Abs(temp - mymed)*10);
                 score += 10 - Math.Min(10, temp);
             }
-            
-            if(!onebutton)
+
+            //Stats session = new MashAttack.Stats((long)mashes.totalTime, mashes.count, (long)mashes.upTotal, (long)mashes.downTotal, 1, (long)score);
+            //Console.WriteLine(usersBox.SelectedValue.ToString());
+            //Console.WriteLine(configBox.SelectedValue.ToString());
+            //session.UpdateAll(usersBox.SelectedValue.ToString(), configBox.SelectedValue.ToString(), "SNES");
+
+            if (!onebutton)
             {
                 mymed = 1000.0 / mashes2.GetMedian();
 
@@ -442,8 +514,10 @@ namespace MashAttack
                 }
             }
 
-            scoreLabel.Content = String.Format("Score: {0}",score);
-            scoreLabel.Visibility = Visibility.Visible;
+            //scoreLabel.Content = String.Format("Score: {0}",score);
+            //scoreLabel.Visibility = Visibility.Visible;
+
+            return score;
         }
 
         private string FormatStrings(double num)
@@ -475,6 +549,11 @@ namespace MashAttack
         }
 
         private void playerUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerUpdate();
+        }
+
+        private void PlayerUpdate()
         {
             usersBox.Items.Clear();
             List<string> users = sheets.GetUsernames();
---
+++ MashAttack/Stats.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MashAttack
+{
+    class Stats
+    {
+        readonly string BASE = ".\\Stats\\";
+
+        public long totaltime = 0;
+        public long totalcount = 0;
+        public long totalup = 0;
+        public long totaldown = 0;
+        public long totalsessions = 0;
+        public long totalscore = 0;
+        public long totalmedian = 0;
+
+        public Stats()
+        {
+            totaltime = 0;
+            totalcount = 0;
+            totalup = 0;
+            totaldown = 0;
+            totalsessions = 0;
+            totalscore = 0;
+            totalmedian = 0;
+        }
+        public Stats(long newtotal, long newcount, long newup, long newdown, long newsessions, long newscore, long newmedian)
+        {
+            totaltime = newtotal;
+            totalcount = newcount;
+            totalup = newup;
+            totaldown = newdown;
+            totalsessions = newsessions;
+            totalscore = newscore;
+            totalmedian = newmedian;
+        }
+
+        public void UpdateAll(bool onebutton, string player, string mode, string input)
+        {
+            Stats tempStats = new Stats();
+            if (onebutton)
+            {
+                tempStats.LoadFile(BASE+"OneButton\\", "global.txt");
+                tempStats += this;
+                tempStats.SaveFile(BASE+"OneButton\\", "global.txt");
+
+                tempStats.LoadFile(BASE + "OneButton\\", input + ".txt");
+                tempStats += this;
+                tempStats.SaveFile(BASE + "OneButton\\", input + ".txt");
+
+                tempStats.LoadFile(BASE + "OneButton\\", mode + ".txt");
+                tempStats += this;
+                tempStats.SaveFile(BASE + "OneButton\\", mode + ".txt");
+
+                tempStats.LoadFile(BASE + "OneButton\\" + player + "\\", player + ".txt");
+                tempStats += this;
+                tempStats.SaveFile(BASE + "OneButton\\" + player + "\\", player + ".txt");
+
+                tempStats.LoadFile(BASE + "OneButton\\" + player + "\\", mode + ".txt");
+                tempStats += this;
+                tempStats.SaveFile(BASE + "OneButton\\" + player + "\\", mode + ".txt");
+            }
+            else
+            {
+                tempStats.LoadFile(BASE + "TwoButton\\", "global.txt");
+                tempStats += this;
+                tempStats.SaveFile(BASE + "TwoButton\\", "global.txt");
+
+                tempStats.LoadFile(BASE + "TwoButton\\", input + ".txt");
+                tempStats += this;
+                tempStats.SaveFile(BASE + "TwoButton\\", input + ".txt");
+
+                tempStats.LoadFile(BASE + "TwoButton\\", mode + ".txt");
+                tempStats += this;
+                tempStats.SaveFile(BASE + "TwoButton\\", mode + ".txt");
+
+                tempStats.LoadFile(BASE + "TwoButton\\" + player + "\\", player + ".txt");
+                tempStats += this;
+                tempStats.SaveFile(BASE + "TwoButton\\" + player + "\\", player + ".txt");
+
+                tempStats.LoadFile(BASE + "TwoButton\\" + player + "\\", mode + ".txt");
+                tempStats += this;
+                tempStats.SaveFile(BASE + "TwoButton\\" + player + "\\", mode + ".txt");
+            }
+        }
+
+        private void LoadFile(string path, string filename)
+        {
+            Directory.CreateDirectory(path);
+            if (File.Exists(path+filename))
+            {
+                StreamReader sr = new StreamReader(path+filename);
+
+                totaltime = Convert.ToInt64(sr.ReadLine());
+                totalcount = Convert.ToInt64(sr.ReadLine());
+                totalup = Convert.ToInt64(sr.ReadLine());
+                totaldown = Convert.ToInt64(sr.ReadLine());
+                totalsessions = Convert.ToInt64(sr.ReadLine());
+                totalscore = Convert.ToInt64(sr.ReadLine());
+                totalmedian = Convert.ToInt64(sr.ReadLine());
+                sr.Close();
+
+                //return new Stats(newtotal, newcount, newup, newdown, newsessions, newscore);
+            }
+            else
+            {
+                totaltime = 0;
+                totalcount = 0;
+                totalup = 0;
+                totaldown = 0;
+                totalsessions = 0;
+                totalscore = 0;
+                totalmedian = 0;
+            }
+        }
+
+        public Stats GetGlobal(bool onebutton)
+        {
+            Stats temp = new Stats();
+            if (onebutton)
+                temp.LoadFile(BASE + "OneButton\\", "global.txt");
+            else
+                temp.LoadFile(BASE + "TwoButton\\", "global.txt");
+
+            return temp;
+        }
+
+        public Stats GetPlayerData(bool onebutton, string player, string mode)
+        {
+            Stats temp = new Stats();
+            if (onebutton)
+                temp.LoadFile(BASE + "OneButton\\" + player + "\\", mode + ".txt");
+            else
+                temp.LoadFile(BASE + "TwoButton\\" + player + "\\", mode + ".txt");
+
+            return temp;
+        }
+
+        private void SaveFile(string path, string filename)
+        {
+            Directory.CreateDirectory(path);
+            StreamWriter sw = new StreamWriter(path+filename,false);
+            sw.WriteLine(String.Format("{0}", totaltime));
+            sw.WriteLine(String.Format("{0}", totalcount));
+            sw.WriteLine(String.Format("{0}", totalup));
+            sw.WriteLine(String.Format("{0}", totaldown));
+            sw.WriteLine(String.Format("{0}", totalsessions));
+            sw.WriteLine(String.Format("{0}", totalscore));
+            sw.WriteLine(String.Format("{0}", totalmedian));
+            sw.Close();
+
+        }
+
+        public static Stats operator +(Stats s1, Stats s2)
+        {
+            return new MashAttack.Stats(s1.totaltime + s2.totaltime, s1.totalcount + s2.totalcount, s1.totalup + s2.totalup, s1.totaldown + s2.totaldown, s1.totalsessions + s2.totalsessions, s1.totalscore + s2.totalscore, s1.totalmedian+s2.totalmedian);
+        }
+    }
+}
